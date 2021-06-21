e5bccecbc783d312dfc7fdc5cd859063a18be9c8
jonas <jonas.skott.nielsen@live.dk>
2021-06-21 17:28:05 +0200

    .

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
-const fs = require('fs')
-const csv = require('csv-parser')
+const csv = require('csv-parser');
+const fs = require('fs');
+var resultsFromCsv = [];
+var itemFromShopify = [];
+
+
+function updateInventory (){
+
+fs.createReadStream('InventoryList.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    const item = {
+        sku: `${row["LivingSport SKU no."]}`,
+        Quantity: `${row["Quantity"]}`,
+        Arrival: `${row["Next Arrival"]}`,
+        brand: `${row["Brand"]}`
+    };
+    resultsFromCsv.push(item);
+  })
+  .on('end', () => {
+    //console.log(resultsFromCsv)
+    resultsFromCsv.forEach(element => {
+      console.log(element);
+      console.log('--------------')
+    });
+  });
+}
+updateInventory();
