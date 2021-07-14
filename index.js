ea4ec7e74403434870cc947fd5589d9976f955b2
jonas nielsen <jonas18@student.sdu.dk>
2021-07-14 13:14:31 +0200

    messing around with cron jobs

---
+++ index.js
@@ -0,0 +1,23 @@
+const cron = require('node-cron');
+const express = require('express');
+var msg = require('./fun.js');
+app = express();
+
+// Schedule tasks to be run on the server.
+/*
+                ┌──────────────── second (optional) (Valid range 0-59)
+                | ┌────────────── minute (Valid range: 0-59)
+                | | ┌──────────── hour (valid range: 0-23)
+                | | | ┌────────── day of the month (Valid range: 1-31)
+                | | | | ┌──────── month (Valid range: 1-12)
+                | | | | | ┌────── day of the week (valid range: 0-7)
+                | | | | | | 
+                | | | | | |
+                * * * * * *
+*/
+   cron.schedule('* * * * *', function() {
+  console.log('running a task every minute');
+  console.log(msg);
+});
+
+app.listen(3000);
